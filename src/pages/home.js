2c6f0d33b3fb4f004631446b55182ce8f8098983
nitish <nitish.hardeniya@happay.in>
2019-08-02 22:24:28 +0530

    basic API integration

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
 export default class home extends Component {
+
+	constructor(){
+		super();
+		this.state = {
+			movies:[]
+		}
+	}
+
+	componentDidMount(){
+		fetch('https://api.themoviedb.org/3/movie/popular?api_key=d6dadf6fd94626fca2e1aab19a6cf5e0').then((data)=>data.json()).then(data=>{
+			this.setState({
+				movies:data.results
+			})
+		})
+	}
+
 	render() {
 		return (
-			<div>Movie spot</div>
-		);
+			<React.Fragment>
+			{this.state.movies.map((movie)=>{
+							return <div>{movie.title}</div>
+						})}
+			</React.Fragment>);
 	}
 }
