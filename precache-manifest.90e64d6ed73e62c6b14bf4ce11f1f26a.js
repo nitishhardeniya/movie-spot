5740442e5b29a5e057d3d3eacfd87ad4b87f4dfa
nitish <nitish.hardeniya@happay.in>
2019-08-06 00:16:16 +0530

    Updates

---
+++ precache-manifest.90e64d6ed73e62c6b14bf4ce11f1f26a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89531e48a20938b76b49a7540162fa9",
+    "url": "/movie-spot/index.html"
+  },
+  {
+    "revision": "11b2dd172b7f40ad0e61",
+    "url": "/movie-spot/static/css/main.7749f690.chunk.css"
+  },
+  {
+    "revision": "a71a85c41cf8947669dc",
+    "url": "/movie-spot/static/js/2.e7e95ebe.chunk.js"
+  },
+  {
+    "revision": "11b2dd172b7f40ad0e61",
+    "url": "/movie-spot/static/js/main.50604516.chunk.js"
+  },
+  {
+    "revision": "fe21ef2bf9496570604a",
+    "url": "/movie-spot/static/js/runtime~main.7923435e.js"
+  }
+]);
