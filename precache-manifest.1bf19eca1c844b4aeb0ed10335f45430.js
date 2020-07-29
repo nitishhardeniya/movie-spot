dcd9454dd5c68e596a12637f3ce9090e6f47bf24
nitish <nitish.hardeniya@happay.in>
2020-07-29 12:16:44 +0530

    Updates

---
+++ precache-manifest.1bf19eca1c844b4aeb0ed10335f45430.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ea523e734c0c938295aeb92a7289b2",
+    "url": "/movie-spot/index.html"
+  },
+  {
+    "revision": "765d04fd8a66a08f2010",
+    "url": "/movie-spot/static/css/main.a95554d9.chunk.css"
+  },
+  {
+    "revision": "5ec216b801d836fe221c",
+    "url": "/movie-spot/static/js/2.c6ca1331.chunk.js"
+  },
+  {
+    "revision": "765d04fd8a66a08f2010",
+    "url": "/movie-spot/static/js/main.495709d5.chunk.js"
+  },
+  {
+    "revision": "fe21ef2bf9496570604a",
+    "url": "/movie-spot/static/js/runtime~main.7923435e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-spot/precache-manifest.10fa6dc459a36c1f7d582a616c4ec73a.js"
+  "/movie-spot/precache-manifest.1bf19eca1c844b4aeb0ed10335f45430.js"
 );
 
 self.addEventListener('message', (event) => {
